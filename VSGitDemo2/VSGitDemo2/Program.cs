cb1f0f88c0c383746d6fb8fb29e0f6d8e6911689
Student <Student@VMMB310-66>
2018-01-19 12:49:57 +0200

    EDITED

---
+++ VSGitDemo2/VSGitDemo2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VSGitDemo2
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            string[] colours = { "Coffee", "Tea", "Orange Juice", "Milk", "Water", "Liquor", "Apple Juice", "Energy Drink" };
+            Random rnd = new Random();
+            int index = rnd.Next(0, colours.Length);
+            Console.WriteLine("Have we earned a {0} break?", colours[index]);
+        }
+    }
+
+}
+}
