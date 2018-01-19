83b29c65fca0047df239851d487f2580879b4f90
Student <Student@VMMB310-66>
2018-01-19 12:52:53 +0200

    Removed liquor

--- VSGitDemo2/VSGitDemo2/Program.cs
+++ VSGitDemo2/VSGitDemo2/Program.cs
@@ -10,7 +10,7 @@ namespace VSGitDemo2
     {
         public static void Main()
         {
-            string[] colours = { "Coffee", "Tea", "Orange Juice", "Milk", "Water", "Liquor", "Apple Juice", "Energy Drink" };
+            string[] colours = { "Coffee", "Tea", "Orange Juice", "Milk", "Water", "Apple Juice", "Energy Drink" };
             Random rnd = new Random();
             int index = rnd.Next(0, colours.Length);
             Console.WriteLine("Have we earned a {0} break?", colours[index]);
